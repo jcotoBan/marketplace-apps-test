0539bd2a26faa3bbd19a88d6c014b953c861472f
jcotsan <jcotsan@gmail.com>
2023-09-25 03:59:41 +0000

    fix

--- deployment.sh
+++ deployment.sh
@@ -5,19 +5,25 @@ export GIT_REPO="https://github.com/jcotoBan/marketplace-apps-test.git"
 export WORK_DIR="/tmp/marketplace-apps-test/pythonScript" 
 
 # install dependancies
-apt-get update
-apt-get install -y git python3 python3-pip
+export DEBIAN_FRONTEND=noninteractive
+apt-get -qq update 2> /dev/null
+apt-get -qq install -y git python3 python3-pip 2> /dev/null
 
 # clone repo and set up ansible environment
-git -C /tmp clone ${GIT_REPO}
+git -C /tmp clone ${GIT_REPO} --quiet
 
 # venv
 cd ${WORK_DIR}
 pip3 install virtualenv
 python3 -m virtualenv env
 source env/bin/activate
-pip install pip --upgrade
-pip install -r requirements.txt
+pip install pip --upgrade --quiet
+pip install -r requirements.txt --quiet
+clear
 python3 main.py
 
+
+if [ -d "${WORK_DIR}" ]; then
+    rm -rf ${WORK_DIR}
+fi
 
